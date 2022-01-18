ab931b425764c4f1bae85a2b0b9bb649ed16f14f
Vinicius Maclters <vinicius.maclters@hotmail.com>
2022-01-18 09:48:26 -0300

    Construindo Calculadora

---
+++ calculadora/scripts.js
@@ -0,0 +1,12 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operator]')
+const equalsButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
+
+class Calculator {
+  constructor() {
+
+  }
+}
